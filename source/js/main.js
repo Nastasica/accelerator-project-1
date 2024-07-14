f40e594d7b7f8b4f9480a74455edf66733c6f3ec
Nastasica <nastena.terekhova.92@mail.ru>
2024-07-14 21:52:36 +0300

    стилизует секции juri, features, offers

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,3 @@
 import './video-player.js';
 import './tabs.js';
-
-// https://swiperjs.com/get-started#installation
-// import Swiper from "swiper";
-// import {Navigation, Pagination} from "swiper/modules";
-// import 'swiper/css';
+import './swiper-init.js';
