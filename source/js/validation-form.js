a522bc8f4062af5d1de90c10f23c6f89ed36c8bf
Nastasica <nastena.terekhova.92@mail.ru>
2024-07-27 02:06:08 +0300

    дорабатывает tablet по pixelPerfect

--- source/js/validation-form.js
+++ source/js/validation-form.js
@@ -1,38 +1,37 @@
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('freeLessonForm');
 
-  form.addEventListener('submit', (event) => {
-    event.preventDefault();
+  if (form) {
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
 
-    const nameInput = document.getElementById('name');
-    const phoneInput = document.getElementById('phone');
-    const nameError = document.getElementById('name-error');
-    const phoneError = document.getElementById('phone-error');
+      const nameInput = document.getElementById('name');
+      const phoneInput = document.getElementById('phone');
+      const nameError = document.getElementById('name-error');
+      const phoneError = document.getElementById('phone-error');
 
-    let isValid = true;
+      let isValid = true;
 
-    // Очистка предыдущих сообщений об ошибках
-    nameInput.classList.remove('error');
-    nameError.classList.remove('active');
-    phoneInput.classList.remove('error');
-    phoneError.classList.remove('active');
+      nameInput.classList.remove('error');
+      nameError.classList.remove('active');
+      phoneInput.classList.remove('error');
+      phoneError.classList.remove('active');
 
-    // Валидация поля имени
-    if (!/^[A-Za-zА-Яа-яЁё\- ]+$/.test(nameInput.value)) {
-      nameInput.classList.add('error');
-      nameError.classList.add('active');
-      isValid = false;
-    }
+      if (!/^[A-Za-zА-Яа-яЁё\- ]+$/.test(nameInput.value)) {
+        nameInput.classList.add('error');
+        nameError.classList.add('active');
+        isValid = false;
+      }
 
-    // Валидация поля телефона
-    if (!/^\+?[\d ()-]+$/.test(phoneInput.value)) {
-      phoneInput.classList.add('error');
-      phoneError.classList.add('active');
-      isValid = false;
-    }
+      if (!/^\+?[\d ()-]+$/.test(phoneInput.value)) {
+        phoneInput.classList.add('error');
+        phoneError.classList.add('active');
+        isValid = false;
+      }
 
-    if (isValid) {
-      form.submit();
-    }
-  });
+      if (isValid) {
+        form.submit();
+      }
+    });
+  }
 });
